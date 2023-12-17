441f16d6ac0ff9822c92513d47c86b3b306ad93b
nauxscript <nauxscript@gmail.com>
2023-12-17 21:58:23 +0800

    feat: basic map render

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,48 @@
 import { A } from 'solid-start'
 
+type DotState = 0 | 1 | 2 | 3 | 4 
+
 export default function Home() {
+  // const map = Array(16).fill(Array(16).fill(1))
+  // console.log(map);
+  const rawMapData: DotState[][] = [
+    Array(8).fill(0),
+    Array(8).fill(0),
+    Array(8).fill(0),
+    Array(8).fill(0),
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 3, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 2, 4, 2, 3, 0],
+    [0, 3, 1, 2, 1, 0, 0, 0],
+    [0, 0, 0, 0, 2, 0, 0, 0],
+    [0, 0, 0, 0, 3, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    Array(8).fill(0),
+    Array(8).fill(0),
+    Array(8).fill(0),
+    Array(8).fill(0),
+  ]
+  console.log(rawMapData);
+  const map = rawMapData.map(col => {
+    return [...Array(4).fill(0), ...col, ...Array(4).fill(0)]
+  }) 
+
+  const drawMap = ['🪨', '', '📦', '🎯', '🚶‍♂️', '💠']
+
+  console.log(map);  
+
   return (
-    <main px-4 py-10 text-center>
-      <div text-4xl>
-        <div i-carbon-mountain inline-block=""></div>
+    <main px-4 py-10 flex justify-center>
+      <div w-192 grid grid-cols-16 grid-rows-16 gap-0 rounded-md overflow-hidden  bg-slate-200>
+        {map.map((col, colIndex) => {
+          return col.map((row, rowIndex) => (
+            <div lh-10 text-center text-10 w-12 h-12>
+              {drawMap[row]}
+            </div>
+          ))
+        })}
       </div>
-      <p>
-        <em text-sm text-gray-500>Opinionated Vite Starter Template for <a href="https://www.solidjs.com/" text-gray-500 hover:text-dark><strong>Solid</strong></a></em>
-      </p>
-      <nav text-xl mt-6 >
-        <A href="/">
-          <div i-carbon-mountain inline-block="" text-gray hover:text-dark mx-1></div>
-        </A>
-        <A href="/about">
-          <div i-carbon-content-view inline-block="" text-gray hover:text-dark mx-1></div>
-        </A>
-        <A href="https://github.com/Nauxscript/vitesse-lite-solid">
-          <div i-carbon-logo-github inline-block="" text-gray hover:text-dark mx-1></div>
-        </A>
-      </nav>
     </main>
   )
 }
